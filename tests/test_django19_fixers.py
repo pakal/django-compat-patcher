64c2528ee4172358dced707c5cc7c6d50b4a7939
Pascal Chambon <chambon.pascal@gmail.com>
2016-10-04 14:51:07 +0200

    Add test for request.REQUEST.

--- tests/test_django19_fixers.py
+++ tests/test_django19_fixers.py
@@ -6,6 +6,7 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "test_project.settings"
 
 import _test_utilities  # initializes django
 
+
 def test_keep_templatetags_future_url():
     from compat import render_to_string
 
@@ -14,3 +15,20 @@ def test_keep_templatetags_future_url():
 
     rendered = render_to_string('core_tags/test_defaulttags_url.html')
     assert rendered.strip() == "/homepage/"
+
+
+def test_keep_request_post_get_mergedict():
+    from django.test.client import RequestFactory
+    factory = RequestFactory()
+
+    request = factory.get('/homepage/?abcd=66')
+    assert "abc" not in request.REQUEST
+
+    request = factory.get('/homepage/?abc')
+    assert request.REQUEST["abc"] == ""
+
+    request = factory.get('/homepage/?abc=6%26')
+    assert request.REQUEST["abc"] == "6&"
+
+    request = factory.post('/homepage/?abc=66', data=dict(abc="aju"))
+    assert request.REQUEST["abc"] == "aju"  # POST takes precedence over GET
