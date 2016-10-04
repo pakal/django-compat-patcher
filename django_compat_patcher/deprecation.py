98eba29fe7afd783837822636bb702f81976e5c6
Pascal Chambon <chambon.pascal@gmail.com>
2016-10-04 14:23:53 +0200

    Add keep_request_post_get_mergedict to keep request.REQUEST.

---
+++ django_compat_patcher/deprecation.py
@@ -0,0 +1,21 @@
+
+
+class RemovedInDjango21Warning(PendingDeprecationWarning):
+    pass
+
+class RemovedInDjango20Warning(DeprecationWarning):
+    pass
+
+class RemovedInDjango19Warning(DeprecationWarning):
+    pass
+
+class RemovedInDjango19Warning(DeprecationWarning):
+    pass
+
+class RemovedInDjango18Warning(DeprecationWarning):
+    pass
+
+class RemovedInDjango17Warning(DeprecationWarning):
+    pass
+
+RemovedInNextVersionWarning = RemovedInDjango21Warning
--- django_compat_patcher/fixers/django19.py
+++ django_compat_patcher/fixers/django19.py
@@ -1,6 +1,8 @@
 
-
+import warnings
 from ..registry import register_backwards_compatibility_fixer
+
+from ..deprecation import *
 
 
 @register_backwards_compatibility_fixer()
@@ -10,3 +12,18 @@ def keep_templatetags_future_url(utils):
     new_tag = utils.inject_function_alias(defaulttags, "url",
                                           future, "url")
     future.register.tag(new_tag)
+
+
+@register_backwards_compatibility_fixer()
+def keep_request_post_get_mergedict(utils):
+    from django.core.handlers.wsgi import WSGIRequest
+    from .. import datastructures
+    def _get_request_compat(self):
+        warnings.warn('`request.REQUEST` is deprecated, use `request.GET` or '
+                      '`request.POST` instead.', RemovedInDjango19Warning, 2)
+        if not hasattr(self, '_request'):
+            self._request = datastructures.MergeDict(self.POST, self.GET)
+        return self._request
+
+    WSGIRequest._get_request = _get_request_compat
+    WSGIRequest.REQUEST = property(_get_request_compat)
