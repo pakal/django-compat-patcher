6b5fcab3e673348e6bc46e632812642a7cbb38cc
Raphaël Gomès <alphare33@gmail.com>
2016-10-13 13:11:03 +0200

    Remove superfluous link to local datastructures

--- django_compat_patcher/fixers/django19.py
+++ django_compat_patcher/fixers/django19.py
@@ -89,12 +89,12 @@ def fix_deletion_request_post_get_mergedict(utils):  # TODO rename to fit new gu
     "and POST (the latter has precedence)."
 
     from django.core.handlers.wsgi import WSGIRequest
-    from ..removed.django19 import datastructures
+    from django.utils.datastructures import MergeDict  # Depends on a previous fixer
     def _get_request_compat(self):
         utils.emit_warning('`request.REQUEST` is deprecated, use `request.GET` or '
                            '`request.POST` instead.', RemovedInDjango19Warning, 2)
         if not hasattr(self, '_request'):
-            self._request = datastructures.MergeDict(self.POST, self.GET)
+            self._request = MergeDict(self.POST, self.GET)
         return self._request
 
     utils.inject_method(WSGIRequest, "_get_request", _get_request_compat)
