ef8fd775abaf46f3b50450101d9e13bc868311d2
Pascal Chambon <chambon.pascal@gmail.com>
2016-10-13 14:25:56 +0200

    Fix sorting of django19 fixers.

--- django_compat_patcher/fixers/django19.py
+++ django_compat_patcher/fixers/django19.py
@@ -12,74 +12,6 @@ django19_bc_fixer = partial(register_compatibility_fixer,
                             fixer_applied_from_django="1.9")
 
 
-@django19_bc_fixer()
-def fix_deletion_templatetags_future_url(utils):  # TODO rename to fit new guidelines
-    "Preserve the `url` tag in the `future` templatetags library."
-    from django.template import defaulttags
-    from django.templatetags import future
-    new_tag = utils.inject_function_alias(defaulttags, "url",
-                                          future, "url")
-    future.register.tag(new_tag)
-
-
-def keep_templatetags_future_ssi(utils):
-    "Preserve the `ssi` tag in the `future` templatetags library."
-    from django.template import defaulttags
-    from django.templatetags import future
-    new_tag = utils.inject_function_alias(defaulttags, "ssi",
-                                          future, "ssi")
-    future.register.tag(new_tag)
-
-
-@django19_bc_fixer()
-def fix_deletion_forms_fields_IPAddressField(utils):
-    """Preserve the IPAddressField model field, now superseded by GenericIPAddressField"""
-    import django.forms.fields
-    from django.forms.fields import CharField
-    from django.core import validators
-
-    class IPAddressFieldCompat(CharField):
-        default_validators = [validators.validate_ipv4_address]
-
-        def __init__(self, *args, **kwargs):
-            utils.emit_warning("IPAddressField has been deprecated. Use GenericIPAddressField instead.",
-                                RemovedInDjango19Warning)
-            super(IPAddressFieldCompat, self).__init__(*args, **kwargs)
-
-        def to_python(self, value):
-            if value in self.empty_values:
-                return ''
-            return value.strip()
-
-    utils.inject_class(django.forms.fields, "IPAddressField", IPAddressFieldCompat)
-
-    from django.db.models.fields import IPAddressField as OriginalIPAddressField
-
-    def formfield_compat(self, **kwargs):
-        defaults = {'form_class': django.forms.fields.IPAddressField}
-        defaults.update(kwargs)
-        return super(OriginalIPAddressField, self).formfield(**defaults)
-
-    utils.inject_attribute(OriginalIPAddressField, "formfield", formfield_compat)
-
-
-@django19_bc_fixer()
-def keep_request_post_get_mergedict(utils):
-    "Preserve the `request.REQUEST` attribute, merging parameters from GET "
-    "and POST (the latter has precedence)."
-
-    from django.core.handlers.wsgi import WSGIRequest
-    from .. import datastructures
-    def _get_request_compat(self):
-        utils.emit_warning('`request.REQUEST` is deprecated, use `request.GET` or '
-                       '`request.POST` instead.', RemovedInDjango19Warning, 2)
-        if not hasattr(self, '_request'):
-            self._request = datastructures.MergeDict(self.POST, self.GET)
-        return self._request
-
-    utils.inject_method(WSGIRequest, "_get_request", _get_request_compat)
-    utils.inject_attribute(WSGIRequest, "REQUEST", property(_get_request_compat))
-
 
 @django19_bc_fixer()
 def fix_deletion_utils_datastructures_SortedDict(utils):
@@ -173,3 +105,55 @@ def fix_deletion_contrib_admin_ModelAdmin_get_formsets(utils):
 
     utils.inject_method(ModelAdmin, "_get_formsets", _get_formsets_compat)
     utils.inject_method(ModelAdmin, "get_formsets", get_formsets_compat)
+
+
+@django19_bc_fixer()
+def fix_deletion_templatetags_future_url(utils):  # TODO rename to fit new guidelines
+    "Preserve the `url` tag in the `future` templatetags library."
+    from django.template import defaulttags
+    from django.templatetags import future
+    new_tag = utils.inject_function_alias(defaulttags, "url",
+                                          future, "url")
+    future.register.tag(new_tag)
+
+
+def keep_templatetags_future_ssi(utils):
+    "Preserve the `ssi` tag in the `future` templatetags library."
+    from django.template import defaulttags
+    from django.templatetags import future
+    new_tag = utils.inject_function_alias(defaulttags, "ssi",
+                                          future, "ssi")
+    future.register.tag(new_tag)
+
+
+@django19_bc_fixer()
+def fix_deletion_forms_fields_IPAddressField(utils):
+    """Preserve the IPAddressField model field, now superseded by GenericIPAddressField"""
+    import django.forms.fields
+    from django.forms.fields import CharField
+    from django.core import validators
+
+    class IPAddressFieldCompat(CharField):
+        default_validators = [validators.validate_ipv4_address]
+
+        def __init__(self, *args, **kwargs):
+            utils.emit_warning("IPAddressField has been deprecated. Use GenericIPAddressField instead.",
+                                RemovedInDjango19Warning)
+            super(IPAddressFieldCompat, self).__init__(*args, **kwargs)
+
+        def to_python(self, value):
+            if value in self.empty_values:
+                return ''
+            return value.strip()
+
+    utils.inject_class(django.forms.fields, "IPAddressField", IPAddressFieldCompat)
+
+    from django.db.models.fields import IPAddressField as OriginalIPAddressField
+
+    def formfield_compat(self, **kwargs):
+        defaults = {'form_class': django.forms.fields.IPAddressField}
+        defaults.update(kwargs)
+        return super(OriginalIPAddressField, self).formfield(**defaults)
+
+    utils.inject_attribute(OriginalIPAddressField, "formfield", formfield_compat)
+
