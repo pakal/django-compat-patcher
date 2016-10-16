724079b28d46e1bf8695bb3f616c818d3e16b412
Raphaël Gomès <alphare33@gmail.com>
2016-10-16 17:06:16 +0200

    Add plugin to conftest.py to ensure all fixers have a test

--- conftest.py
+++ conftest.py
@@ -1,1 +1,15 @@
 import _test_utilities  # initializes django
+from django_compat_patcher.registry import get_all_fixers
+
+
+def ensure_all_fixers_have_a_test(items):
+    all_fixers = get_all_fixers()
+    all_tests_names = [test.name for test in items]
+    for fixer in all_fixers:
+        expected_test_name = "test_{}".format(fixer['fixer_callable'].__name__)
+        if expected_test_name not in all_tests_names:
+            raise RuntimeError("No test written for {} fixer '{}'".format(fixer['fixer_family'].title(), fixer['fixer_callable'].__name__))
+
+
+def pytest_collection_modifyitems(config, items):  # FIXME figure out how to test this...
+    ensure_all_fixers_have_a_test(items)
