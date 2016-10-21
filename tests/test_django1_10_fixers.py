c335e6d08f9ec1e1e9a6c8bbdf02a0cc929e5924
Pascal Chambon <chambon.pascal@gmail.com>
2016-10-21 15:32:15 +0200

    Update first django1_10 fixer tests.

--- tests/test_django1_10_fixers.py
+++ tests/test_django1_10_fixers.py
@@ -6,19 +6,12 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 def test_fix_deletion_templatetags_future():
+    from compat import render_to_string
     from django.templatetags.future import cycle, firstof
-    assert False
+    rendered = render_to_string('core_tags/test_future_cycle_and_firstof.html')
+    assert rendered.strip() == 'row1\nA'
 
 
 def test_fix_deletion_template_defaulttags_ssi():
     # already tested in "test_fix_deletion_templatetags_future_ssi()"
-    pass
-    ''' TODO TEST THIS
-    @ignore_warnings(category=RemovedInDjango110Warning)
-    @setup({'firstof11': '{% load firstof from future %}{% firstof a b %}'})
-    def test_firstof11(self):
-        output = self.engine.render_to_string('firstof11', {'a': '<', 'b': '>'})
-        self.assertEqual(output, '&lt;')
-    DjangoTemplates.fromstring
-   '''
-
+    from django.template.defaulttags import ssi
