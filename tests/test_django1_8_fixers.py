8f07749589013c2715333381fff2bbb82c68312f
Pascal Chambon <chambon.pascal@gmail.com>
2016-12-04 19:04:22 +0100

    Add test_django1_8_fixers.py

---
+++ tests/test_django1_8_fixers.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os, sys
+
+import _test_utilities
+
+def test_fix_deletion_contrib_comments():
+
+    from django.contrib import comments
+    import django.contrib.comments
+
+    import django.contrib.comments.urls
+    assert isinstance(django.contrib.comments.urls.urlpatterns, list)
+
+    from django.contrib.comments.views import comments as comments_views
+    assert callable(comments_views.post_comment)
