555bc7ef7fcf81b05d73be12f96740765695b5d3
Pascal Chambon <chambon.pascal@gmail.com>
2016-12-06 19:56:28 +0100

    Fix naming of variables in import_proxifier.py, and add enrich_import_error().

--- django_compat_patcher/import_proxifier.py
+++ django_compat_patcher/import_proxifier.py
@@ -19,11 +19,26 @@ Beware about not creating loops with your aliases, as this could trigger infinit
 """
 
 import os, sys
-import importlib
+import importlib, contextlib
 
 
 # maps ALIASES to REAL MODULES
 MODULES_ALIASES_REGISTRY = []
+
+
+@contextlib.contextmanager
+def enrich_import_error(alias_name):
+    try:
+        yield
+    except ImportError as e:
+        #print(vars(e), str(e), dir(e), e.args)
+        # ImportError exception has different structure between py2k and py3k
+        enrich_message = lambda msg: "%s (when loading alias name '%s')" % (msg, alias_name)
+        if hasattr(e, "msg"):  # py3k
+            e.msg = enrich_message(e.msg)
+        else:  # py2k, the "message" attribute is actually ignored by __str__
+            e.args = (enrich_message(e.args[0]),)
+        raise
 
 
 def register_module_alias(alias_name, real_name):
@@ -37,7 +52,11 @@ def register_module_alias(alias_name, real_name):
     return False
 
 
-def _get_module_alias(fullname):
+def _get_module_alias_real_name(fullname):
+    """
+    returns the real name of module (when fullname is an alias name)
+    or None.
+    """
     for k, v in MODULES_ALIASES_REGISTRY:
         if (k == fullname) or fullname.startswith(k +"."):
             return fullname.replace(k, v)
@@ -56,27 +75,33 @@ except ImportError:
 
 
     class AliasingLoader(object):
-        def __init__(self, alias_name):
+
+        def __init__(self, real_name, alias_name):
+            self.real_name = real_name
             self.alias_name = alias_name
+
         def load_module(self, name):
             if name in sys.modules:
                 return sys.modules[name]  # shortcut
             # we let the standard machinery handle sys.modules, module attrs etc.
-            module = importlib.import_module(self.alias_name, package=None)
+            with enrich_import_error(self.alias_name):
+                module = importlib.import_module(self.real_name, package=None)
             sys.modules[name] = module  # cached
             return module
 
 
     class ModuleAliasFinder(object):
+
         @classmethod
         def find_module(self, fullname, *args, **kwargs):
 
             #print("OldMetaPathFinder FINDMODULE", fullname, args, kwargs)
 
-            alias_name = _get_module_alias(fullname)
-            if alias_name is None:
-                return None  # no alias module is known
-            return AliasingLoader(alias_name)
+            real_name = _get_module_alias_real_name(fullname)
+            if real_name is None:
+                return None  # no aliased module is known
+            return AliasingLoader(real_name=real_name, alias_name=fullname)
+
 
 else:
 
@@ -89,20 +114,22 @@ else:
 
         target_spec_backup = None
 
-        def __init__(self, alias_name):
+        def __init__(self, real_name, alias_name):
+            self.real_name = real_name
             self.alias_name = alias_name
 
         def create_module(self, spec):
             # we do the real loading of aliased module here
-            module = importlib.import_module(self.alias_name, package=None)
-            assert module.__name__ == self.alias_name, module.__name__
+            with enrich_import_error(self.alias_name):
+                module = importlib.import_module(self.real_name, package=None)
+            assert module.__name__ == self.real_name, module.__name__
             self.target_spec_backup = module.__spec__
             return module
 
         def exec_module(self, module):
             # __name__ was overridden by init_module_attrs(_force_name=True)
-            assert module.__name__ != self.alias_name, module.__name__
-            module.__name__ = self.alias_name
+            assert module.__name__ != self.real_name, module.__name__
+            module.__name__ = self.real_name
             assert module.__spec__.origin == "alias", module.__spec__  # well overridden
             assert module.__spec__.loader_state["aliased_spec"] is None
             assert self.target_spec_backup, self.target_spec_backup
@@ -121,18 +148,18 @@ else:
 
             #print("MetaPathFinder FINDSPEC", fullname, args, kwargs)
 
-            alias_name = _get_module_alias(fullname)
-            if alias_name is None:
-                return None  # no alias module is known
+            real_name = _get_module_alias_real_name(fullname)
+            if real_name is None:
+                return None  # no aliased module is known
 
             # TODO emit warnings/logging
 
-            alias_loader = AliasingLoader(alias_name=alias_name)
+            alias_loader = AliasingLoader(real_name=real_name, alias_name=fullname)
 
             spec = importlib.machinery.ModuleSpec(name=fullname,
                                            loader=alias_loader,
                                            origin="alias",
-                                           loader_state={"aliased_name": alias_name,
+                                           loader_state={"aliased_name": fullname,
                                                          "aliased_spec": None},
                                            is_package=True)  # in doubt, assume...
 
@@ -160,18 +187,19 @@ if __name__ == "__main__":
     install_import_proxifier()
     install_import_proxifier()  # idempotent
 
-    register_module_alias("json.comments", "json.tool")
+    register_module_alias("json.tool_alias", "json.tool")
     register_module_alias("mylogging", "logging")
     register_module_alias("mylogging", "logging")
     register_module_alias("infinite_recursion", "infinite_recursion2")
     register_module_alias("infinite_recursion2", "infinite_recursion")
+    register_module_alias("unexisting_alias", "unexisting_module")
 
     fullname = "logging.handlers"
     print ("ALREADY THERE:", fullname, fullname in sys.modules)
 
-    import json.comments
-    assert sys.modules["json.comments"]
-    assert json.comments
+    import json.tool_alias
+    assert sys.modules["json.tool_alias"]
+    assert json.tool_alias
 
     fullname = "json.comments"
     print ("ALREADY THERE:", fullname, fullname in sys.modules)
@@ -181,13 +209,14 @@ if __name__ == "__main__":
 
     import json.tool
     if is_new_style_proxifier:
-        print ("JSON TOOL SPEC", json.tool.__spec__.loader_state)
+        print ("JSON TOOL SPEC loader_state:", json.tool.__spec__.loader_state)
 
-    assert json.comments is json.tool, (json.comments,  json.tool)
+    assert json.tool_alias is json.tool, (json.tool_alias,  json.tool)
     assert json.tool.__name__ == "json.tool", json.tool.__name__
     if is_new_style_proxifier:
         assert json.tool.__spec__.origin == "alias", json.tool.__spec__.origin
-    
+        assert json.tool.__spec__.name == "json.tool_alias"
+        assert json.tool.__spec__.loader_state["aliased_spec"].name == "json.tool"
 
     from mylogging import config
     assert config.dictConfig
@@ -204,3 +233,21 @@ if __name__ == "__main__":
         pass
     else:
         raise RuntimeError("Import should have led to infinite recursion")
+
+    try:
+        import unexisting_alias
+    except ImportError as e:
+        assert "unexisting_alias" in str(e), (str(e), vars(e))
+        assert "unexisting_module" in str(e), str(e)
+    else:
+        raise RuntimeError("import error noy raised")
+
+    try:
+        import unexisting_module
+    except ImportError as e:
+        assert "unexisting_alias" not in str(e), str(e)
+        assert "unexisting_module" in str(e), str(e)
+    else:
+        raise RuntimeError("import error noy raised")
+
+    print("\n*** Tests were successful ***")
