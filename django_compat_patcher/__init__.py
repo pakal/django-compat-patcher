ef7e53a77711b524974ea404a50ac8c4f46345c5
Pascal Chambon <chambon.pascal@gmail.com>
2016-12-08 19:43:55 +0100

    Precise the handling of Django settings by DCP.

--- django_compat_patcher/__init__.py
+++ django_compat_patcher/__init__.py
@@ -3,11 +3,3 @@ from __future__ import absolute_import, print_function, unicode_literals
 from . import fixers, utilities, deprecation, registry
 from .patcher import patch
 
-
-'''
-if not hasattr(django, "setup"):
-    def setup_retrocompat():
-        pass
-    django.setup = setup_retrocompat
-'''
-
