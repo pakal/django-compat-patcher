a397dc9418f2e92f888e35c02594f1ada03bd1a0
Pascal Chambon <chambon.pascal@gmail.com>
2019-06-14 23:25:06 +0200

    Use new generic_patch_software() from compat-patcher.

--- django_compat_patcher/__init__.py
+++ django_compat_patcher/__init__.py
@@ -1,26 +1,26 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+from compat_patcher import generic_patch_software
+
 
 def patch(settings=None):
     """Load every dependency, and apply registered fixers according to provided settings (or Django settings as a fallback)."""
 
+    from .registry import django_patching_registry
+    from .deprecation import warnings as warnings_proxy
+
     from .config import DjangoConfigProvider
     from .utilities import DjangoPatchingUtilities
-    from .registry import DjangoPatchingRegistry
     from .runner import DjangoPatchingRunner
 
-    config_provider = DjangoConfigProvider(settings=settings)
-
-    patching_utilities = DjangoPatchingUtilities(config_provider=config_provider)
-
-    from . import deprecation  # Immediately finish setting up this module
-    deprecation.warnings.set_patching_utilities(patching_utilities)
-
     from . import fixers  # Force-load every fixer submodule
-    from .registry import django_patching_registry
 
-    django_patching_runner = DjangoPatchingRunner(config_provider=config_provider,
-                                                  patching_utilities=patching_utilities,
-                                                  patching_registry=django_patching_registry,)
-    django_patching_runner.patch_software()
+    django_config_provider = DjangoConfigProvider(settings=settings)
 
+    generic_patch_software(
+        config_provider=django_config_provider,
+        patching_registry=django_patching_registry,
+        patching_utilities_class=DjangoPatchingUtilities,
+        patching_runner_class=DjangoPatchingRunner,
+        warnings_proxy=warnings_proxy,
+    )
