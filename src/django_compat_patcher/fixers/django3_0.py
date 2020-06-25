d07bede690b3a60a3909c7d0bbb6e5771e76aaf2
Pascal Chambon <chambon.pascal@gmail.com>
2020-06-25 22:45:38 +0200

    Add fix_deletion_utils_upath_npath_abspathu() fixer.

--- src/django_compat_patcher/fixers/django3_0.py
+++ src/django_compat_patcher/fixers/django3_0.py
@@ -19,3 +19,25 @@ def fix_deletion_utils_six(utils):
     import six
     utils.inject_module("django.utils.six", six)
 
+
+@django1_30_bc_fixer()
+def fix_deletion_utils_upath_npath_abspathu(utils):
+    """Preserve python2 path normalization functions."""
+    
+    from os.path import abspath as abspathu  # For backwards-compatibility in Django 2.0
+
+    def upath(path):
+        """Always return a unicode path (did something for Python 2)."""
+        return path
+
+    def npath(path):
+        """
+        Always return a native path, that is unicode on Python 3 and bytestring on
+        Python 2. Noop for Python 3.
+        """
+        return path
+
+    from django.utils import _os
+    utils.inject_callable(_os, "abspathu", abspathu)
+    utils.inject_callable(_os, "upath", upath)
+    utils.inject_callable(_os, "npath", npath)
--- tests/test_django3_0_fixers.py
+++ tests/test_django3_0_fixers.py
@@ -7,3 +7,11 @@ def test_fix_deletion_utils_six():
     assert django.utils.six.string_types
     assert six2.string_types
 
+
+def test_fix_deletion_utils_upath_npath_abspathu():
+    from os.path import abspath
+    from django.utils._os import abspathu, upath, npath
+
+    assert abspathu(".") == abspath(".")
+    assert upath("/something/file.txt") == "/something/file.txt"  # No-op
+    assert npath("/something2/file.txt") == "/something2/file.txt"  # No-op
