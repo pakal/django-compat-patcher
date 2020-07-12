5b525f508dc75702f47a22172be9ae2a380da549
Pascal Chambon <chambon.pascal@gmail.com>
2020-07-12 09:05:49 +0200

    Add missing test_project/utilities.py

---
+++ tests/test_project/utilities.py
@@ -0,0 +1,11 @@
+
+from django.core.cache.backends.locmem import LocMemCache
+
+class CloseHookMixin:
+    closed = False
+
+    def close(self, **kwargs):
+        self.closed = True
+
+class CacheClass(CloseHookMixin, LocMemCache):
+    pass
