be9b1b4b7cc1df2e3359fd00450dc19828e6b25c
Pascal Chambon <chambon.pascal@gmail.com>
2020-08-22 11:37:12 +0200

    Improve and test the cascading of DCP settings.

--- src/django_compat_patcher/config.py
+++ src/django_compat_patcher/config.py
@@ -18,8 +18,7 @@ class DjangoSettingsProvider(object):
             self._settings = settings
         else:
             from django.conf import settings as django_settings
-
-            self._settings = django_settings.__dict__
+            self._settings = django_settings
 
         from . import default_settings as default_settings
 
@@ -32,11 +31,14 @@ class DjangoSettingsProvider(object):
 
         value = getenv(item, default=None)
         if value is not None:
+            try:
                 return json.loads(value)
+            except ValueError as exc:
+                raise ValueError("Abnormal json value %r for env variable %s (%s)" % (value, item, exc))
 
         try:
-            value = self._settings[item]
-        except KeyError as e:
+            value = self._settings[item] if isinstance(self._settings, dict) else getattr(self._settings, item)
+        except (KeyError, AttributeError) as e:
             # print("EXCEPTION IN get_patcher_setting", name, e)
             value = self._fallback_settings[item]
         return value
--- tests/test_scaffolding.py
+++ tests/test_scaffolding.py
@@ -1,6 +1,9 @@
 import os
 
 import pytest
+from django.conf import settings
+from django_compat_patcher.config import DjangoSettingsProvider
+
 from compat_patcher_core.scaffolding import ensure_no_stdlib_warnings
 
 
@@ -25,3 +28,56 @@ def test_no_package_shadowing_in_tox():
     package_dir = os.path.dirname(os.path.abspath(django_compat_patcher.__file__))
     if os.getenv("INSIDE_TOX_FOR_DCP") and ".tox" not in package_dir:
         raise RuntimeError("Wrong django_compat_patcher package used in Tox")
+
+
+def test_dcp_settings_overrides():
+
+    old_environ = os.environ.copy()
+
+    assert not hasattr(settings, "DCP_INCLUDE_FIXER_IDS")
+    settings.DCP_INCLUDE_FIXER_IDS = ["some_django_setting_for_included_ids"]
+
+    try:
+
+        django_settings_provider = DjangoSettingsProvider(settings=dict(DCP_INCLUDE_FIXER_FAMILIES="*"))
+        assert django_settings_provider["include_fixer_families"] == "*"
+        assert django_settings_provider["include_fixer_ids"] == "*"  # DEFAULT DCP VALUE, no fallback to Django-level setting
+
+        django_settings_provider = DjangoSettingsProvider(settings=None)
+        assert django_settings_provider["include_fixer_ids"] == ["some_django_setting_for_included_ids"]
+
+        os.environ.update(
+            DCP_INCLUDE_FIXER_IDS = '["some_included_id"]',
+            DCP_INCLUDE_FIXER_FAMILIES = '["some_included_family"]',
+
+            DCP_EXCLUDE_FIXER_IDS = '["some_excluded_id"]',
+            DCP_EXCLUDE_FIXER_FAMILIES = '["some_excluded_family"]',
+
+            DCP_PATCH_INJECTED_OBJECTS = 'true',
+
+            DCP_LOGGING_LEVEL = '"DEBUG"',
+            DCP_ENABLE_WARNINGS = 'false',
+        )
+
+        assert django_settings_provider["include_fixer_ids"] == ["some_included_id"]
+        assert django_settings_provider["include_fixer_families"] == ["some_included_family"]
+        assert django_settings_provider["exclude_fixer_ids"] == ["some_excluded_id"]
+        assert django_settings_provider["exclude_fixer_families"] == ["some_excluded_family"]
+        assert django_settings_provider["patch_injected_objects"] == True
+        assert django_settings_provider["logging_level"] == "DEBUG"
+        assert django_settings_provider["enable_warnings"] == False
+
+        del os.environ["DCP_INCLUDE_FIXER_IDS"]
+        assert django_settings_provider["include_fixer_ids"] == ["some_django_setting_for_included_ids"]  # Fallback to django-level setting
+
+        os.environ.update(
+            DCP_INCLUDE_FIXER_IDS = "['some_included_id']"  # NOT real json due to quotes
+        )
+        with pytest.raises(ValueError, match="Abnormal json"):
+            django_settings_provider["include_fixer_ids"]
+    finally:
+        os.environ.clear()
+        os.environ.update(old_environ)
+        del settings.DCP_INCLUDE_FIXER_IDS
+
+
