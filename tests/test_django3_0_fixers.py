3e1d57f42dead8a6131b1ab96fc2121acd8a0506
Pascal Chambon <chambon.pascal@gmail.com>
2021-01-19 22:30:47 +0100

    Fix python2 compatibility regarding unicode

--- tests/test_django3_0_fixers.py
+++ tests/test_django3_0_fixers.py
@@ -20,7 +20,7 @@ def test_fix_deletion_utils_six():
 
     # Old versions of compat-patcher-core crashed due to wrong module names set by six._importer
     from django.utils.six.moves.urllib.parse import urlencode
-    assert urlencode(dict(name="hêllo")) == "name=h%C3%AAllo"
+    assert urlencode(dict(name="h\xc3llo")) == "name=h%C3%83llo" if six.PY3 else "name=h%C3llo"
 
 
 def test_fix_deletion_utils_upath_npath_abspathu():
